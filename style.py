b01e652531e02bb3b00048a16b11fbcf13fdc157
wjbotham <wjbotham@gmail.com>
2012-06-24 15:00:22 -0500

    Add missing style.py file

---
+++ style.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.font.init()
+FONT = pygame.font.SysFont(None, 14)
+
+OPACITY = 191
+
+BLACK       = (  0,  0,  0)
+WHITE       = (255,255,255)
+LIGHT_GRAY  = (127,127,127)
+GREEN       = (  0,255,  0)
+DARK_GREEN  = (  0,159,  0)
+LIGHT_GREEN = (127,255,127)
+RED         = (255,  0,  0)
