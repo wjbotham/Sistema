79f2e5ef8ebf3e34efcde74104ff570865f4bb7e
Wesley Botham <wjbotham@gmail.com>
2013-12-21 20:20:16 -0600

    Add a few more unit tests for PhysicsSnapshot

--- test/test_physics_snapshot.py
+++ test/test_physics_snapshot.py
@@ -7,6 +7,34 @@ class TestPhysicsSnapshotFunctions(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_init(self):
+        ps = PhysicsSnapshot()
+        self.assertTrue(not ps.ready)
+        self.assertTrue(not ps.okay_to_delete)
+
+    def test_record(self):
+        ps = PhysicsSnapshot()
+        self.assertEqual(ps.kinematics, {})
+        ps.record("TEST_BODY", (0,1,2), (3,2,1))
+        k = ps.kinematics["TEST_BODY"]
+        self.assertEqual(k.position, (0,1,2))
+        self.assertEqual(k.velocity, (3,2,1))
+
+    def test_okay_to_delete(self):
+        ps = PhysicsSnapshot()
+        ps.game_loop_finish()
+        self.assertTrue(not ps.okay_to_delete)
+        ps.graphics_loop_finish()
+        self.assertTrue(ps.okay_to_delete)
+        del ps
+
+        ps = PhysicsSnapshot()
+        ps.graphics_loop_finish()
+        self.assertTrue(not ps.okay_to_delete)
+        ps.game_loop_finish()
+        self.assertTrue(ps.okay_to_delete)
+        del ps
+
     '''
     this test makes sure that PhysicsSnapshot blocks correctly when other
     methods call PhysicsSnapshot.acquire()
